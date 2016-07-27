63f3bc4f31c9be490a316f4f7c1e0add248a2e3d
Zahidul Haque <iisowrovii@gmail.com>
2016-07-27 16:11:36 +0600

    docker installer refactored

--- customs/docker.sh
+++ customs/docker.sh
@@ -1,29 +1,24 @@
 #!/usr/bin/env bash
+function _install_docker {
+	echo "########## Trying to install docker"
+	if which docker > /dev/null ; then
+		echo "########## docker already installed. Skipping ..."
+	else
+		sudo apt-get purge lxc-docker -y --force-yes
+		sudo apt-get install docker-engine -y --force-yes
+		sudo groupadd docker
+		sudo gpasswd -a ${USER} docker
+		sudo service docker restart
+		newgrp docker
+	fi
+}
+
 if [[ -f /etc/apt/sources.list.d/docker.list ]]; then
-    echo "########## PPA already added. Skipping ..."
-    echo "########## Trying to install docker"
-    if which docker > /dev/null ; then
-        echo "########## docker already installed. Skipping ..."
-    else
-        sudo apt-get purge lxc-docker -y --force-yes
-	sudo apt-get install docker-engine -y --force-yes
-	sudo groupadd docker
-	sudo gpasswd -a ${USER} docker
-	sudo service docker restart
-	newgrp docker
-    fi
+	echo "########## PPA already added. Skipping ..."
 else
-    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A91
-    echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-    sudo apt-get update -y --force-yes
-    if which docker > /dev/null ; then
-	echo "########## docker already installed. Skipping ..."
-    else
-	sudo apt-get purge lxc-docker -y --force-yes
-	sudo apt-get install docker-engine -y --force-yes
-	sudo groupadd docker
-	sudo gpasswd -a ${USER} docker
-	sudo service docker restart
-	newgrp docker
-    fi
+	sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A91
+	sudo cp files/docker.list /etc/apt/sources.list.d/docker.list
+	sudo apt-get update -y --force-yes
 fi
+
+_install_docker
