c756524e837e55ebf80536bb7f1228ae99d1a0c7
indrajit <eendroroy@gmail.com>
2016-08-03 18:04:54 +0600

    update sources

--- customs/add-getdeb-repo.sh
+++ customs/add-getdeb-repo.sh
@@ -3,6 +3,7 @@ if [[ -f /etc/apt/sources.list.d/getdeb.list ]]; then
   echo "########## getded pa already added. Skipping ..."
 else
   echo "########## Adding getdeb ppa ..."
+  echo "http://github.com/eendroroy/loki-bootstrap.git" | sudo tee /etc/apt/sources.list/getdeb.list
   sudo cp sources.list/getdeb.list.xenial /etc/apt/sources.list.d/getdeb.list
   sudo apt-get update -y --force-yes
 fi
--- customs/virtualbox5.sh
+++ customs/virtualbox5.sh
@@ -3,8 +3,8 @@ if [[ -f /etc/apt/sources.list.d/virtualbox.list ]]; then
   echo "########## PPA already added. Skipping ..."
 else
   echo "########## Adding virtualbox ppa ..."
-  #echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-  sudo cp sources.list/virtualbox.list.xenial /etc/apt/sources.list.d/virtualbox.list
+  echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+  # sudo cp sources.list/virtualbox.list.xenial /etc/apt/sources.list.d/virtualbox.list
   wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
   sudo apt-get update -y
 fi
