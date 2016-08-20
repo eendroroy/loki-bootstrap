38177a1af2db542eb9d4449ba3bc8c9385130b53
indrajit <eendroroy@gmail.com>
2016-08-21 01:32:25 +0600

    update nvidia.sh

--- customs/nvidia.sh
+++ customs/nvidia.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo apt-get update -y
-sudo apt-get install -y --force-yes nvidia-graphics-drivers-370
+sudo apt-get install -y --force-yes nvidia-370
