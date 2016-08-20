8e4b97b471197d06c9e9c66f1fe506d33d834fcb
indrajit <eendroroy@gmail.com>
2016-08-21 04:19:51 +0600

    update nvidia.sh

--- customs/nvidia.sh
+++ customs/nvidia.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+sudo add-apt-repository --remove -y ppa:graphics-drivers/ppa
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo apt-get update -y
 sudo apt-get install -y --force-yes nvidia-370
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
