b2e9135ae4ad631f128b35b79c83b88514fa08f5
indrajit <eendroroy@gmail.com>
2016-08-24 23:58:42 +0600

    added google ppa

--- customs/google-apps.sh
+++ customs/google-apps.sh
@@ -1,27 +1,21 @@
 #/usr/bin/env bash
 
-if [[ -f /etc/apt/sources.list.d/google.list ]]; then
-  if cat /etc/apt/sources.list.d/google.list | grep chrome > /dev/null ; then
-    echo "########## chrome ppa already added. Skipping ..."
-  else
-    echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google.list
-  fi
-
-  if cat /etc/apt/sources.list.d/google.list | grep musicmanager > /dev/null ; then
-    echo "########## musicmanager ppa already added. Skipping ..."
-  else
-    echo "deb [arch=amd64] http://dl.google.com/linux/musicmanager/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google.list
-  fi
-  
-  if cat /etc/apt/sources.list.d/google.list | grep talkplugin > /dev/null ; then
-    echo "########## talkplugin ppa already added. Skipping ..."
-  else
-    echo "deb [arch=amd64] http://dl.google.com/linux/talkplugin/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google.list
-  fi
+if [[ -f /etc/apt/sources.list.d/google-chrome.list ]]; then
+  echo "########## chrome ppa already added. Skipping ..."
 else
-  echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google.list
-  echo "deb [arch=amd64] http://dl.google.com/linux/musicmanager/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google.list
-  echo "deb [arch=amd64] http://dl.google.com/linux/talkplugin/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google.list
+  echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-chrome.list
+fi
+
+if [[ -f /etc/apt/sources.list.d/google-musicmanager.list ]]; then
+  echo "########## musicmanager ppa already added. Skipping ..."
+else
+  echo "deb [arch=amd64] http://dl.google.com/linux/musicmanager/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-musicmanager.list
+fi
+  
+if [[ -f /etc/apt/sources.list.d/google-talkplugin.list ]]; then
+  echo "########## talkplugin ppa already added. Skipping ..."
+else
+  echo "deb [arch=amd64] http://dl.google.com/linux/talkplugin/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-talkplugin.list
 fi
 
 sudo apt-get update -y
