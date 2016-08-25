10c0f2c2558cd41e967fef3a98f3e655808aa691
Indrajit <eendroroy@users.noreply.github.com>
2016-08-25 12:28:50 +0600

    Update neovim.sh

--- customs/neovim.sh
+++ customs/neovim.sh
@@ -6,8 +6,8 @@ else
   sudo apt install software-properties-common
   sudo add-apt-repository --remove -y ppa:neovim-ppa/unstable
   sudo add-apt-repository -y ppa:neovim-ppa/unstable
-  sudo apt update
-  sudo apt install -y neovim
+  sudo apt-get update
+  sudo apt-get install -y neovim
   sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
   sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
   sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
