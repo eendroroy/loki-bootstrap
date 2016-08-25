d2299dc9a04f07a0ceff28650474eb190b6cfb3f
Indrajit <eendroroy@users.noreply.github.com>
2016-08-25 12:28:27 +0600

    Update zsh.sh

--- customs/zsh.sh
+++ customs/zsh.sh
@@ -3,6 +3,6 @@ if which zsh > /dev/null; then
   echo "########## zsh already installed. Skipping ..."
 else
   echo "########## Installing zsh ..."
-  sudo apt install -y --force-yes zsh
+  sudo apt-get install -y --force-yes zsh
   chsh -s $(which zsh)
 fi
