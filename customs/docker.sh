8a155339f45c8592bc631b6b11b3930e305fb466
indrajit <eendroroy@gmail.com>
2016-10-20 12:09:28 +0600

    updated docker repo

--- customs/docker.sh
+++ customs/docker.sh
@@ -6,7 +6,7 @@ if [[ -f /etc/apt/sources.list.d/docker.list ]]; then
 else
   echo "########## adding docker ppa ..."
   sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A91
-  echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+  echo "deb http://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
   # sudo cp sources.list/docker.list.xenial /etc/apt/sources.list.d/docker.list
   sudo apt-get update -y --force-yes
 fi
