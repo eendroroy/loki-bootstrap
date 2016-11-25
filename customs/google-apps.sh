e0b1924eb41da1fb68f89d510818cb723c4af0b6
indrajit <eendroroy@gmail.com>
2016-11-26 01:49:25 +0600

    remove google music manager beta

--- customs/google-apps.sh
+++ customs/google-apps.sh
@@ -3,25 +3,20 @@
 if [[ -f /etc/apt/sources.list.d/google-chrome.list ]]; then
   echo "########## chrome ppa already added. Skipping ..."
 else
+  echo "########## installing google-chrome ppa ...."
   echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-chrome.list
 fi
 
-if [[ -f /etc/apt/sources.list.d/google-musicmanager.list ]]; then
-  echo "########## musicmanager ppa already added. Skipping ..."
-else
-  echo "deb [arch=amd64] http://dl.google.com/linux/musicmanager/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-musicmanager.list
-fi
-  
 if [[ -f /etc/apt/sources.list.d/google-talkplugin.list ]]; then
   echo "########## talkplugin ppa already added. Skipping ..."
 else
+  echo "########## installing google-talkplugin ppa ...."
   echo "deb [arch=amd64] http://dl.google.com/linux/talkplugin/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-talkplugin.list
 fi
 
 sudo apt-get update -y
 sudo apt-get install -y --force-yes \
   google-chrome-stable \
-  google-musicmanager-beta \
   google-talkplugin
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
