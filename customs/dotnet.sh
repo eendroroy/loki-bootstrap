8407a4b311aabbc4e0da54dc0ec4592ac1ca6555
indrajit <eendroroy@gmail.com>
2016-12-04 06:33:49 +0600

    update dotnet core version

--- customs/dotnet.sh
+++ customs/dotnet.sh
@@ -7,5 +7,5 @@ else
   # sudo cp sources.list/dotnetdev.list.xenial /etc/apt/sources.list.d/dotnetdev.list
   sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
   sudo apt-get update -y
-  sudo apt-get install -y --force-yes dotnet-dev-1.0.0-preview2-003121
+  sudo apt-get install -y --force-yes dotnet-dev-1.0.0-preview2-003156
 fi
