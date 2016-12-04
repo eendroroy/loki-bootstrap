2f93d24ce12b02a933030c8a86ce258d5586fd87
indrajit <eendroroy@gmail.com>
2016-12-04 06:20:32 +0600

    update docker-compose version

--- customs/docker-compose.sh
+++ customs/docker-compose.sh
@@ -3,7 +3,7 @@ if which docker-compose > /dev/null; then
   echo "########## docker-compose already installed. Skipping ..."
 else
   echo "########## Installing docker-compose ..."
-  curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > docker-compose
   sudo mv docker-compose /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
