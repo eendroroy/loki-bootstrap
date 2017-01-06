21c77dba49e969be92c5182ee469e37e0677b579
indrajit <eendroroy@gmail.com>
2017-01-06 17:18:48 +0600

    update exenv installation

--- customs/exenv.sh
+++ customs/exenv.sh
@@ -3,6 +3,7 @@ if [ -d ~/.exenv ]; then
   echo '########## exenv already installed, skipping.'
 else
   echo "########## Installing exenv ..."
+  sudo apt-get install -y --force-yes erlang erlang-doc
   git clone https://github.com/mururu/exenv.git ~/.exenv
   mkdir -p ~/.exenv/plugins
   git clone https://github.com/mururu/elixir-build.git ~/.exenv/plugins/elixir-build
