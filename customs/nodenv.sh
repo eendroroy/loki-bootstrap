93ae1b115da5e9b97502ac4141b63e6eaeeb5a1e
indrajit <eendroroy@gmail.com>
2017-03-03 14:16:36 +0600

    update nodenv installation

--- customs/nodenv.sh
+++ customs/nodenv.sh
@@ -5,6 +5,7 @@ else
   echo "########## Installing node ..."
   sudo apt-get install -y --force-yes libssl-dev libreadline-dev zlib1g-dev git
   git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+  cd ~/.nodenv && src/configure && make -C src
   mkdir -p ~/.nodenv/plugins/
   git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
 fi
