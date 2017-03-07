4adf32893d386226820ad53158045a4236ef578e
indrajit <eendroroy@gmail.com>
2017-03-08 02:01:22 +0600

    update pyenv git url

--- customs/pyenv.sh
+++ customs/pyenv.sh
@@ -6,5 +6,5 @@ else
   sudo apt-get install -y --force-yes libssl-dev libreadline-dev zlib1g-dev git
   sudo apt-get install -y --force-yes build-essential checkinstall
   sudo apt-get install -y --force-yes libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-  git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 fi
