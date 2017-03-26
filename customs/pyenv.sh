3c8947b46f8e1d8eac8c5b4d49e4d701551b4091
indrajit <eendroroy@gmail.com>
2017-03-27 01:46:28 +0600

    added pyenv-ccache pyenv-version-ext plugins

--- customs/pyenv.sh
+++ customs/pyenv.sh
@@ -7,4 +7,9 @@ else
   sudo apt-get install -y --force-yes build-essential checkinstall
   sudo apt-get install -y --force-yes libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+  mkdir -p ~/.pyenv/plugins
+  pushd ~/.pyenv/plugins
+  git clone https://github.com/pyenv/pyenv-ccache.git
+  git clone https://github.com/pyenv/pyenv-version-ext.git
+  popd
 fi
