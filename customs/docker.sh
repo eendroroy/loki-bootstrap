a3cb07af437079a3e1c72e53dc9bd16c0960afdb
indrajit <eendroroy@gmail.com>
2017-05-22 15:59:22 +0600

    update docker installation script

--- customs/docker.sh
+++ customs/docker.sh
@@ -3,20 +3,14 @@
 release_name=`lsb_release -csu 2> /dev/null || lsb_release -cs 2> /dev/null`
 [[ "$release_name" = "" ]] && release_name="xenial"
 
-# adding ppa
-if [[ -f /etc/apt/sources.list.d/docker.list ]]; then
-  echo "########## PPA already added. Skipping ..."
-else
-  echo "########## adding docker ppa ..."
-  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A91
-  echo "deb http://apt.dockerproject.org/repo ubuntu-${release_name} main" | sudo tee /etc/apt/sources.list.d/docker.list
-  sudo apt-get update -y --force-yes
-fi
-
 # installing
 if which docker > /dev/null ; then
   echo "########## docker already installed. Skipping ..."
 else
+  echo "########## adding docker ppa ..."
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${release_name} stable"
+  sudo apt-get update -y
   echo "########## Installing docker ..."
   echo "########## Creating docker data directory in home partition ..."
   sudo mkdir -p /home/docker_data_dir
@@ -26,7 +20,7 @@ else
   sudo apt-get install apt-transport-https ca-certificates -y --force-yes
   sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y --force-yes
   sudo apt-get purge lxc-docker -y --force-yes
-  sudo apt-get install docker-engine -y --force-yes
+  sudo apt-get install docker-ce -y --force-yes
   sudo groupadd docker
   sudo gpasswd -a ${USER} docker
   sudo service docker restart
