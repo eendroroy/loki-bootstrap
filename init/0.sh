5b74ab7a8e3c59e7e7b748e68b5ca6cfbff408e1
indrajit <eendroroy@gmail.com>
2017-09-20 17:55:46 +0600

    fix 0.sh

--- init/0.sh
+++ init/0.sh
@@ -5,7 +5,9 @@ sudo sed -i "s|bd.||g" /etc/apt/sources.list
 sudo dpkg --add-architecture i386
 
 # init update
-update
-upgrade
-clean
-
+sudo apt-get update -y
+sudo apt-get upgrade -y --force-yes
+sudo apt-get dist-upgrade -y --force-yes
+sudo apt-get autoremove -y --force-yes
+sudo apt-get autoclean -y --force-yes
+sudo apt-get install -f -y --force-yes
