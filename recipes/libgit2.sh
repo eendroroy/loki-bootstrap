fd49f4fa1c47ad0f24d992c04b9dd01e129abc85
indrajit <eendroroy@gmail.com>
2017-12-04 15:23:26 +0600

    libgit2 update

--- recipes/libgit2.sh
+++ recipes/libgit2.sh
@@ -4,7 +4,9 @@ cd /tmp
 rm -rf libgit2
 git clone git@github.com:libgit2/libgit2.git
 cd libgit2
+git checkout v0.26.0
 mkdir build && cd build
 cmake ..
-sudo cmake --build . --target install
+sudo cmake --build . --clean-first --target install
+sudo ldconfig
 
