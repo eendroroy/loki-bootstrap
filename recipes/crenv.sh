08cc6637a6aa4aa151d8ecf53f4769cc5506a654
indrajit <eendroroy@gmail.com>
2018-08-20 17:19:43 +0600

    update crenv

--- recipes/crenv.sh
+++ recipes/crenv.sh
@@ -3,13 +3,10 @@ if [ -d ~/.crenv ]; then
   echo '########## crenv already installed, skipping.'
 else
   echo "########## Installing crenv ..."
-  git clone git@github.com:pine/crenv.git ~/.crenv
-  pushd ~/.crenv
-  pushd src/configure && make -C src && popd
-  popd
+  git clone https://github.com/pine/crenv.git ~/.crenv
+  pushd ~/.crenv/src/configure && make -C src && popd
   mkdir -p ~/.crenv/plugins
   pushd ~/.crenv/plugins
-  git clone git@github.com:pine/crystal-build.git
-  git clone git@github.com:crenv/crenv-update.git
-  popd
+  git clone https://github.com/pine/crystal-build.git
+  git clone https://github.com/crenv/crenv-update.git
 fi
