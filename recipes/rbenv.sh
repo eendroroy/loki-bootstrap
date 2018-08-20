e49b326c36d0f5b192850a7730de084560fba769
indrajit <eendroroy@gmail.com>
2018-08-20 17:20:45 +0600

    update rbenv

--- recipes/rbenv.sh
+++ recipes/rbenv.sh
@@ -4,13 +4,11 @@ if [ -d ~/.rbenv ]; then
 else
   echo "########## Installing rbenv ..."
   sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev git
-  git clone git://github.com/rbenv/rbenv.git ~/.rbenv
-  pushd ~/.rbenv
-  pushd src/configure && make -C src && popd
-  popd
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  pushd ~/.rbenv/src/configure && make -C src && popd
   mkdir -p ~/.rbenv/plugins
   pushd ~/.rbenv/plugins
-  git clone git://github.com/rbenv/ruby-build.git
+  git clone https://github.com/rbenv/ruby-build.git
   git clone https://github.com/rbenv/rbenv-default-gems.git
   popd
   echo "bundler" | tee ~/.rbenv/default-gems
