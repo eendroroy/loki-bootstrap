c83351c07595404a3160707c229e626c3d6175e6
indrajit <eendroroy@gmail.com>
2018-12-12 22:19:35 +0600

    mrefactor

--- helpers/ppas.sh
+++ helpers/ppas.sh
@@ -2,7 +2,6 @@
 declare -a ppas=(\
   "snwh/pulp" \
   "noobslab/apps" \
-  # "webupd8team/java" \
   "webupd8team/sublime-text-3" \
   "nomacs/daily" \
   "synapse-core/testing" \
@@ -21,8 +20,6 @@ declare -a ppas=(\
 declare -a packages=(\
   paper-gtk-theme paper-cursor-theme paper-icon-theme \
   indicator-synapse \
-  # oracle-java7-installer oracle-java8-installer \
-  # oracle-java8-set-default \
   sublime-text-installer \
   nomacs \
   synapse \
